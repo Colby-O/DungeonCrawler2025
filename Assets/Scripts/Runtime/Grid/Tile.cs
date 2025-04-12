4a11a68ba3100e86f709bd7fadddb7d8e73d4ba0
smokeythekat <owr5273@gmail.com>
2025-04-11 22:42:28 -0300

    stuffs

--- Assets/Scripts/Runtime/Grid/Tile.cs
+++ Assets/Scripts/Runtime/Grid/Tile.cs
@@ -5,15 +5,21 @@ using PlazmaGames.Runtime.DataStructures;
 
 namespace DC2025
 {
-    public class Tile : MonoBehaviour
-    {
-        [SerializeField] private SerializableDictionary<Direction, bool> _walls;
+	public class Tile : MonoBehaviour
+	{
+		[SerializeField] private SerializableDictionary<Direction, bool> _walls;
+		[SerializeField] private Vector3 _globalPosition;
 
-        public bool HasWallAt(Direction dir)
-        {
-            dir = dir.GetFacingDirection(-transform.rotation.eulerAngles.y);
-            if (_walls.ContainsKey(dir)) return _walls[dir];
-            return false;
-        }
-    }
+		public bool HasWallAt(Direction dir)
+		{
+			dir = dir.GetFacingDirection(-transform.rotation.eulerAngles.y);
+			if (_walls.ContainsKey(dir)) return _walls[dir];
+			return false;
+		}
+
+		private void FixedUpdate()
+		{
+			_globalPosition = transform.position;
+		}
+	}
 }
--- Assets/Scripts/Runtime/MonoSystem/Grid/GridMonoSystem.cs
+++ Assets/Scripts/Runtime/MonoSystem/Grid/GridMonoSystem.cs
@@ -5,48 +5,60 @@ using UnityEngine;
 
 namespace DC2025
 {
-    public class GridMonoSystem : MonoBehaviour, IGridMonoSystem
-    {
-        [SerializeField] private Vector2 _tileSize;
+	public class GridMonoSystem : MonoBehaviour, IGridMonoSystem
+	{
+		[SerializeField] private Vector2 _tileSize;
 
-        [SerializeField] private SerializableDictionary<Vector2Int, Tile> _tiles;
+		[SerializeField] private SerializableDictionary<Vector2Int, Tile> _tiles;
 
-        public Vector2 GetTileSize() => _tileSize;
+		public Vector2 GetTileSize() => _tileSize;
 
-        public Vector2Int WorldToGrid(Vector3 worldPos)
-        {
-            Vector3 clamped = new Vector3(worldPos.x / _tileSize.x, 0, worldPos.z / _tileSize.y);
-            return new Vector2Int(Mathf.FloorToInt(clamped.x), Mathf.FloorToInt(clamped.z));
-        }
+		public Vector2Int WorldToGrid(Vector3 worldPos)
+		{
+			Vector3 clamped = new Vector3(Mathf.Round(worldPos.x) / _tileSize.x, 0, Mathf.Round(worldPos.z) / _tileSize.y);
+			return new Vector2Int(Mathf.FloorToInt(clamped.x), Mathf.FloorToInt(clamped.z));
+		}
 
-        public Vector3 GridToWorld(Vector2Int gridPos)
-        {
-            return new Vector3(gridPos.x * _tileSize.x, 0, gridPos.y * _tileSize.y);
-        }
+		public Vector3 GridToWorld(Vector2Int gridPos)
+		{
+			return new Vector3(gridPos.x * _tileSize.x, 0, gridPos.y * _tileSize.y);
+		}
 
-        public bool CanMoveTo(Vector2Int gridPos, Direction from)
-        {
-            if (_tiles.ContainsKey(gridPos)) return !_tiles[gridPos].HasWallAt(from);
-            return false;
-        }
+		public bool CanMoveTo(Vector2Int gridPos, Direction from)
+		{
+			if (_tiles.ContainsKey(gridPos)) return !_tiles[gridPos].HasWallAt(from);
+			return false;
+		}
 
-        private void RegisterTiles()
-        {
-            Tile[] tiles = FindObjectsByType<Tile>(FindObjectsSortMode.None);
+		private void RegisterTiles()
+		{
+			Tile[] tiles = FindObjectsByType<Tile>(FindObjectsSortMode.None);
 
-            foreach (Tile tile in tiles)
-            {
-                if (tile != null)
-                {
-                    _tiles.Add(WorldToGrid(tile.transform.position), tile);
-                }
-            }
-        }
+			foreach (Tile tile in tiles)
+			{
+				if (tile != null)
+				{
+					Vector2Int gridPos = WorldToGrid(tile.transform.position);
+					if (_tiles.ContainsKey(gridPos))
+					{
+						Debug.Log("-------- ERROR --------------");
+						Debug.Log(_tiles[gridPos].transform);
+						Debug.Log(_tiles[gridPos].transform.position);
+						Debug.Log(tile.transform);
+						Debug.Log(tile.transform.position);
+					}
+					else
+					{
+						_tiles.Add(gridPos, tile);
+					}
+				}
+			}
+		}
 
-        private void Start()
-        {
-            RegisterTiles();
-        }
+		private void Start()
+		{
+			RegisterTiles();
+		}
 
-    }
+	}
 }
