3ee7ab7ea5c92a8270c5f37ec289717b0e0ef754
Colby O'Keefe <okeefeca@icloud.com>
2025-04-11 20:55:11 -0400

    Grid Registeration

--- Assets/Scripts/Runtime/MonoSystem/Grid/GridMonoSystem.cs
+++ Assets/Scripts/Runtime/MonoSystem/Grid/GridMonoSystem.cs
@@ -1,3 +1,6 @@
+using PlazmaGames.Runtime.DataStructures;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DC2025
@@ -5,6 +8,8 @@ namespace DC2025
     public class GridMonoSystem : MonoBehaviour, IGridMonoSystem
     {
         [SerializeField] private Vector2 _tileSize;
+
+        [SerializeField] private SerializableDictionary<Vector2Int, Tile> _tiles;
 
         public Vector2 GetTileSize() => _tileSize;
 
@@ -18,5 +23,30 @@ namespace DC2025
         {
             return new Vector3(gridPos.x * _tileSize.x, 0, gridPos.y * _tileSize.y);
         }
+
+        public bool CanMoveTo(Vector2Int gridPos, Direction from)
+        {
+            if (_tiles.ContainsKey(gridPos)) return !_tiles[gridPos].HasWallAt(from);
+            return false;
+        }
+
+        private void RegisterTiles()
+        {
+            Tile[] tiles = FindObjectsByType<Tile>(FindObjectsSortMode.None);
+
+            foreach (Tile tile in tiles)
+            {
+                if (tile != null)
+                {
+                    _tiles.Add(WorldToGrid(tile.transform.position), tile);
+                }
+            }
+        }
+
+        private void Start()
+        {
+            RegisterTiles();
+        }
+
     }
 }
