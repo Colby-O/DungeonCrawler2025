3b37a95143dc5025b74e328421aa10cba45c616e
smokeythekat <owr5273@gmail.com>
2025-04-19 16:10:12 -0300

    help button

--- Assets/Scripts/Runtime/UI/HelpIcon.cs
+++ Assets/Scripts/Runtime/UI/HelpIcon.cs
@@ -12,7 +12,7 @@ namespace DC2025
         {
             CusorPopup p = GameManager.GetMonoSystem<IInventoryMonoSystem>().GetPopup();
             p.Enable();
-            p.SetText($"<align=center>{_helpTitle}</align>\n\n<align=left>{_helpText}</align>");
+            p.SetText($"<align=center>{_helpTitle}</align>\n<align=left>{_helpText}</align>");
         }
 
         public void OnPointerExit(PointerEventData eventData)
